67ca5a1fdb9f7b0de81222a097961c267c7fcaf4
harifa123 <harifarifu@gmail.com>
2024-02-09 01:04:43 +0530

    msg

---
+++ lib/pages/menu.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class MenuApp extends StatelessWidget {
+  const MenuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen.withOpacity(0.5),
+      appBar: AppBar(
+        title: Text("Covid 19"),
+        backgroundColor: Colors.lightBlueAccent.withOpacity(0.2),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(110),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: ()
+            {
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewPage()));
+            }, child: Text("View Patients")),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: ()
+            {
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>AddPage()));
+            }, child: Text("Add Patients")),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 50,
+              width: 400,
+              child: ElevatedButton(onPressed: (
+                  )
+              {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>SearchPage()));
+              }, child: Text("Search")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
